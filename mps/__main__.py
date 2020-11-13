ad889fa8b3561f63ac19d67eb594263cd90661e9
Henrik Finsberg <henriknf@simula.no>
2020-11-13 08:43:50 +0100

    Update license

--- mps/__main__.py
+++ mps/__main__.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
-# c) 2001-2020 Simula Research Laboratory ALL RIGHTS RESERVED
-#
-# END-USER LICENSE AGREEMENT
-# PLEASE READ THIS DOCUMENT CAREFULLY. By installing or using this
-# software you agree with the terms and conditions of this license
-# agreement. If you do not accept the terms of this license agreement
-# you may not install or use this software.
-
-# Permission to use, copy, modify and distribute any part of this
-# software for non-profit educational and research purposes, without
-# fee, and without a written agreement is hereby granted, provided
-# that the above copyright notice, and this license agreement in its
-# entirety appear in all copies. Those desiring to use this software
-# for commercial purposes should contact Simula Research Laboratory AS:
-# post@simula.no
-#
-# IN NO EVENT SHALL SIMULA RESEARCH LABORATORY BE LIABLE TO ANY PARTY
-# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
-# INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE
-# "MPS" EVEN IF SIMULA RESEARCH LABORATORY HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE. THE SOFTWARE PROVIDED HEREIN IS
-# ON AN "AS IS" BASIS, AND SIMULA RESEARCH LABORATORY HAS NO OBLIGATION
-# TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-# SIMULA RESEARCH LABORATORY MAKES NO REPRESENTATIONS AND EXTENDS NO
-# WARRANTIES OF ANY KIND, EITHER IMPLIED OR EXPRESSED, INCLUDING, BUT
-# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS
 __author__ = "Henrik Finsberg (henriknf@simula.no), 2017--2020"
 __maintainer__ = "Henrik Finsberg"
 __email__ = "henriknf@simula.no"
+__license__ = """
+c) 2001-2020 Simula Research Laboratory ALL RIGHTS RESERVED
+
+END-USER LICENSE AGREEMENT
+PLEASE READ THIS DOCUMENT CAREFULLY. By installing or using this
+software you agree with the terms and conditions of this license
+agreement. If you do not accept the terms of this license agreement
+you may not install or use this software.
+
+Permission to use, copy, modify and distribute any part of this
+software for non-profit educational and research purposes, without
+fee, and without a written agreement is hereby granted, provided
+that the above copyright notice, and this license agreement in its
+entirety appear in all copies. Those desiring to use this software
+for commercial purposes should contact Simula Research Laboratory AS:
+post@simula.no
+
+IN NO EVENT SHALL SIMULA RESEARCH LABORATORY BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE
+"MPS" EVEN IF SIMULA RESEARCH LABORATORY HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE. THE SOFTWARE PROVIDED HEREIN IS
+ON AN "AS IS" BASIS, AND SIMULA RESEARCH LABORATORY HAS NO OBLIGATION
+TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+SIMULA RESEARCH LABORATORY MAKES NO REPRESENTATIONS AND EXTENDS NO
+WARRANTIES OF ANY KIND, EITHER IMPLIED OR EXPRESSED, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS
+"""
 __doc__ = """MPS package for analyzing microphyisological systems
+
 
 Available arguments
 -------------------
@@ -61,6 +64,7 @@ Available options
 
     -h, --help      Show this help
     -v, --version   Show version number
+    -l, --license   Show license
 
 
 Contact
@@ -91,6 +95,9 @@ def main():
         from mps import __version__
 
         print(__version__)
+
+    elif sys.argv[1] == "-l" or sys.argv[1] == "--license":
+        print(__license__)
 
     elif sys.argv[1] == "analyze":
         bin_utils.analyze_mps.run(sys.argv[2:])
